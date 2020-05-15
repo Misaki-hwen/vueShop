81157dea6b8229a5ea1c6370b72d6b66b03a4684
Hwen <jihw@duopu.cn>
2020-05-15 14:53:24 +0800

    引入提示框

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,5 +1,65 @@
 <template>
-    <div>
-        登陆
+    <div class="login-wrap">
+        <el-form 
+        class="login-form"
+        label-preosition="top" 
+        label-width="80px" 
+        :model="formdata">
+        <h2>用户登录</h2>
+            <el-form-item label="用户名">
+                <el-input v-model="formdata.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="formdata.password"></el-input>
+            </el-form-item>
+            <el-button class="login-btn" type="primary" @click.prevent="login()">登录</el-button>
+        </el-form>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            formdata:{
+                username:'',
+                password:''
+            }
+        }
+    },
+    methods: {
+        async login(){
+            let res = await this.$http.post('login',this.formdata)
+            console.log(res);
+            const {data,meta:{msg,status}} = res.data
+            console.log(msg,data)
+            if(status == 200){
+                // this.$router.push({name:'home'})
+                this.$message.success(msg)
+            }else{
+                this.$message.error(msg)
+            }
+        }
+    },
+}
+</script>
+<style>
+.login-wrap{
+    height: 100%;
+    background-color: #324152;
+    /* 弹性和布局 */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+.login-wrap .login-form{
+    width: 400px;
+    background-color: #fff;
+    border-radius:5px;
+    padding: 30px;
+}
+.login-wrap .login-btn{
+    width: 100%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
 import router from './router'
+//全局公共样式
+import '@/assets/css/reset.css'
+
+import MyServerHttp from './plugins/http.js'
+Vue.use(MyServerHttp)
 
 Vue.config.productionTip = false
 
